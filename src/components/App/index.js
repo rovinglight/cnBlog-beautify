f5579fffe88324b6315266d89cef2bffbdb0f0a2
rovinglight <646017375@qq.com>
2017-08-26 16:53:10 +0800

    header

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import Header from '../Header'
 import './App.css';
 
 class App extends Component {
@@ -10,7 +11,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header />
+        <div className='mainContent'>
 
+        </div>
       </div>
     );
   }
---
+++ src/components/Header/index.js
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+import './header.css';
+
+class Header extends Component {
+
+  render () {
+    return (
+      <header>
+        <ul>
+          <li><a href='https://i.cnblogs.com/EditPosts.aspx?opt=1'>新随笔</a></li>
+          <li><a href='https://msg.cnblogs.com/send/%E5%9D%8E%E5%A4%A7%E5%93%88%E7%9A%84%E7%99%BD%E8%A1%A3%E5%A4%A9%E4%BD%BF'>联系</a></li>
+          <li><a href='http://www.cnblogs.com/rovinglight/rss'>订阅</a></li>
+          <li><a href='https://i.cnblogs.com/'>管理</a></li>
+          <li><a href='http://www.cnblogs.com/'>博客园</a></li>
+          <li><a href='http://www.cnblogs.com/rovinglight/'>首页</a></li>
+        </ul>
+      </header>
+    )
+  }
+}
+
+export default Header;
